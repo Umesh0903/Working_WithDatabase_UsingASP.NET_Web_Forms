d3b51cb6635ac1e63e5847c6ce96bfef4aacb42e
umesh patil <patilumesh3689@gmail.com>
2025-06-02 16:49:14 +0530

    Add project files.

---
+++ DatabaseConnectivity/WorkingWithDatabase.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WorkingWithDataBase.aspx.cs" Inherits="DataBaseConnectivity.WorkingWithDataBase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <style>
+       #UserForm {
+       width:300px;
+       }
+   </style>
+
+</head>
+<body>
+    <center>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" BackColor="#99CCFF">
+                <EditRowStyle BackColor="#66FF99" />
+                <HeaderStyle BorderColor="#3399FF" />
+            </asp:GridView>
+        </div>
+
+        <div id="UserForm">
+            <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control mt-2 mb-2" placeholder="Enter ID"></asp:TextBox>
+              <asp:TextBox ID="TextBox2" runat="server" CssClass="form-control mb-2" placeholder="Enter Name"></asp:TextBox>
+              <asp:TextBox ID="TextBox3" runat="server" CssClass="form-control mb-2" placeholder="Enter city"></asp:TextBox>
+
+            <asp:Button ID="Button1" runat="server" Text="Add" CssClass="btn btn-primary ms-2" Width="100" OnClick="Button1_Click"/>
+            <asp:Button ID="Button2" runat="server" Text="Reset" CssClass="btn btn-secondary" Width="100" OnClick="Button2_Click"/> 
+             <br /><br />
+            <asp:Button ID="Button3" runat="server" Text="Delete" CssClass="btn btn-primary ms-2" Width="100" OnClick="Button3_Click"/>
+            <asp:Button ID="Button4" runat="server" Text="Update" CssClass="btn btn-secondary" Width="100" OnClick="Button4_Click"/> 
+
+
+
+
+        </div>
+    </form>
+        </center>
+</body>
+</html>
---
+++ DatabaseConnectivity/WorkingWithDatabase.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DataBaseConnectivity
+{
+    public partial class WorkingWithDataBase : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RJ7FDJE\\SQLEXPRESS;Initial Catalog=mydb;Integrated Security=True;TrustServerCertificate=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        public void getData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from student", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        public void addRecord()
+        {
+            string Rollno, Name, city;
+            Rollno = TextBox1.Text;
+            Name = TextBox2.Text;
+            city = TextBox3.Text;
+            int rno = Convert.ToInt32(Rollno);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Student values(@Rollno,@Name,@city)", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@Rollno",rno);
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@city", city);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record added successfully", "Record added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+        }
+
+        public void DeleteData()
+        {
+            con.Open();
+            var Rollno = Convert.ToInt32(TextBox1.Text);
+            SqlCommand cmd = new SqlCommand("delete from student where  Rollno=@Rollno", con);
+
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@Rollno", Rollno);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record deleted successfully", "Record deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+        }
+
+        public void UpdateData()
+        {
+            string Rollno, Name, city;
+            Rollno = TextBox1.Text;
+            Name = TextBox2.Text;
+            city = TextBox3.Text;
+            var rno = Convert.ToInt32(Rollno);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("update student set Name=@Name, city=@city where Rollno=@Rollno", con);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@Rollno", rno);
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@city", city);
+
+            int count = cmd.ExecuteNonQuery();
+            MessageBox.Show(count + " Record updated successfully", "Record updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            addRecord();
+            getData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = TextBox2.Text = TextBox3.Text = "";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DeleteData();        
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+    }
+}
